5eaf7dfb8f2b394aad5380e4d12aecb0aa2bf7fa
fierperle <56561566+fierperle@users.noreply.github.com>
2022-03-10 11:51:43 +0100

    création du dossier ./minigames et de ./minigames/mastermind.py création de la class Mastermind Création de @mastermind/mastermind/[__init__;generer)

---
+++ minigames/mastermind.py
@@ -0,0 +1,13 @@
+import random as rd
+class Mastermind(Game):
+    def __init__(self, Lst_colorset=["Rouge","Vert","Bleu","Jaune","Blanc","Noir"], Nb_cases=4,Nb_max_coups=12):
+        self.__lst_couleurs = Lst_colorset
+        self.__nb_cases = Nbcases
+        self.__nb_max_coups = Nb_max_coups
+
+        self.__solution = []
+
+    def generer(self):
+        self.__solution = []
+        while len(self.__solution)<= self.__nb_cases:
+            self.__solution.append(rd.randint(0,len(self.__lst_couleurs)-1))
