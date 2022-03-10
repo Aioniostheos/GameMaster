a240330796dd8e57f4bf988814d5436a98dd32fe
fierperle <56561566+fierperle@users.noreply.github.com>
2022-03-10 11:00:26 +0100

    Update de Game/__init__()

--- game.py
+++ game.py
@@ -1,3 +1,12 @@
 class Game:
-    def __init__(self):
-        pass
+    def __init__(self, players, gametype=None, gamevariant=None):
+        self.__players = players
+        self.__gametype = gametype
+        self.__gamevariant = gamevariant
+
+        self.__status = "En Cours"
+
+        self.__creation_date = time.asctime()[4:]
+        self.__end_date = None
+
+        self.save()
