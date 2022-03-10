d39f4946aac43b1ed3553eebc259c5bf6f5f9762
aioniostheos <aioniostheos@laposte.net>
2022-03-10 11:07:19 +0100

    Update @game/Game/__init__() for XML serialization.

--- game.py
+++ game.py
@@ -1,15 +1,17 @@
+from datetime import time
+
+
 class Game:
-    def __init__(self, players, gametype=None, gamevariant=None):
+    def __init__(self, players, game_type=None, game_variant=None,
+                 status="En Cours", creation_date=time.asctime()[4:], end_date=None):
         self.__players = players
-        self.__gametype = gametype
-        self.__gamevariant = gamevariant
+        self.__game_type = game_type
+        self.__game_variant = game_variant
 
-        self.__status = "En Cours"
+        self.__status = status
 
-        self.__creation_date = time.asctime()[4:]
-        self.__end_date = None
-
-        self.save()
+        self.__creation_date = creation_date
+        self.__end_date = end_date
 
     def save(self):
         pass
