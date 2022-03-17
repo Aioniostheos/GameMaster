9fc27af9891f6916d9e541a8a940ea81fd9687cb
fierperle <56561566+fierperle@users.noreply.github.com>
2022-03-17 10:47:54 +0100

    Débug de @mastermind/mastermind création de ./minigames/bataillenavale.py création de testcode.py pour les testes création de @battailnavale/battailnaval/[get_plateau;get_tentative;set_bateau;jouer

--- minigames/mastermind.py
+++ minigames/mastermind.py
@@ -1,11 +1,11 @@
 import random as rd
 
-from ..game import Game
+from game import Game
 
 class Mastermind(Game):
     def __init__(self, Lst_colorset=["Rouge","Vert","Bleu","Jaune","Blanc","Noir"], Nb_cases=4,Nb_max_coups=12):
         self.__lst_couleurs = Lst_colorset
-        self.__nb_cases = Nbcases
+        self.__nb_cases = Nb_cases
         self.__nb_max_coups = Nb_max_coups
         self.__nb_coup = 0
 
@@ -26,24 +26,23 @@ class Mastermind(Game):
 
     def generer(self):
         self.__solution = []
-        while len(self.__solution)<= self.__nb_cases:
-            self.__solution.append(rd.randint(0,len(self.__lst_couleurs)-1))
+        while len(self.__solution)< self.__nb_cases:
+            couleur = self.__lst_couleurs[rd.randint(0,len(self.__lst_couleurs)-1)]
+            self.__solution.append(couleur)
 
     def proposer(self,Proposition): #Proposition est une liste
         self.__lst_coups.append(Proposition)
         self.__nb_coup += 1
-
+        """
+        |
+        |       /!\ A VOIR : COMMENT GERER LA PRESENCE MULTIPLE D'UNE MEME COULEUR
+        |
+        """
         lst_ret = []
         for i,couleur in enumerate(Proposition):
-            if couleur == self.__solution[i]:
-                lst_ret.append("Noir") #bonne couleur et bonne place
+            if couleur == self.__solution[i]: #bonne couleur et bonne place
+                lst_ret.append("Noir")
             else:
                 if couleur in self.__solution:
                     lst_ret.append("Blanc") #bonne couleur, mauvaise place
         return lst_ret
-
-
-if __name__ == '__main__':
-    jeux = Mastermind()
-    jeux.generer()
-    print(jeux.get_solution())
