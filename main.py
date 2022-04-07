099f113f03929fcb90b6ff43f5babb233ed20257
aioniostheos <aioniostheos@laposte.net>
2022-04-07 10:57:19 +0200

    Updated <main>: - Created Interface

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import logging
 import time
+import tkinter as tk
 from pathlib import Path
+from tkinter import ttk
+from tkinter.ttk import Button, Frame, Entry, Spinbox, Label
 
 import game
+
+GAMES = []
 
 path = Path("./data/logs/" + time.asctime().replace(":", "_") + " - debug.log")
 with path.open('w+') as file:
@@ -12,35 +17,37 @@ logging.basicConfig(filename=path, filemode="w", level=logging.DEBUG,
                     format='%(asctime)s %(name)s : %(levelname)s : %(message)s')
 logger = logging.Logger("PyLog")
 
-GAMES = []
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Game Master")
+        logger.info("Initializing App")
+
+        tabcontrol = ttk.Notebook(self)
+
+        frame_gamemaster = ttk.Frame(tabcontrol)
+        frame_minigames = ttk.Frame(tabcontrol)
+        frame_rules = ttk.Frame(tabcontrol)
+
+        Button(frame_minigames, width=10, text="Mastermind").pack()
+        Button(frame_minigames, width=10, text="Bataille Navale").pack()
+
+        tabcontrol.add(frame_gamemaster, text="Game Master")
+        logger.info("Added \"Game Master\" tab")
+        tabcontrol.add(frame_minigames, text="Mini-Jeux")
+        logger.info("Added \"Mini-Jeux\" tab")
+        tabcontrol.add(frame_rules, text="Règles")
+        logger.info("Added \"Règles\" tab")
+        tabcontrol.grid()
+
+    def show(self):
+        self.mainloop()
+        logger.info("Showing App")
+
 
 if __name__ == "__main__":
+    logger = logging.getLogger("PyLog")
     GAMES = game.load_all()
-    print("Bienvenue")
-    end = False
-    while not end:
-        print("Jouer (1)")
-        print("Règle (2)")
-        print("Quitter (3)")
-        command = input()
-        if command == '1':
-            back = False
-            while not back:
-                print("Nouveau (1)")
-                print("Charger (2)")
-                print("Retour (3)")
-                play = input()
-                if play == '1':
-                    print("Not Implemented")
-                elif play == '2':
-                    print("Not Implemented")
-                elif play == '3':
-                    back = True
-                else:
-                    print("Saisie non valide.\n")
-        elif command == '2':
-            print("Not Implemented")
-        elif command == '3':
-            end = True
-        else:
-            print("Saisie non valide.\n")
+
+    App().show()
