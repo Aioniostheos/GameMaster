b573b23d05507d70e85ed9abd72f51bb3da2fd0d
Aioniostheos <aioniostheos@laposte.net>
2022-04-08 20:36:29 +0200

    Updated <mastermind>: - Finished Game Interface Waiting validation...

--- minigames/mastermind.py
+++ minigames/mastermind.py
@@ -1,5 +1,6 @@
 import random
 import tkinter as tk
+from tkinter import messagebox
 from tkinter.ttk import Frame, Entry
 from typing import Dict, List
 
@@ -19,6 +20,8 @@ class Mastermind(Game):
                         "Jaune": "#FFFF00", "Blanc": "#FFFFFF", "Noir": "#000000"}
         self.__lst_couleurs = list(colorset.values())
         self.__nb_coups = 0
+
+        self.__app = None
 
         self.__solution = []
         self.__tentatives = [[] for i in range(12)]
@@ -53,8 +56,9 @@ class Mastermind(Game):
         app.mainloop()
 
     def play(self, app):
+        self.__app = app
         self.generer()
-        app.geometry("175x400")
+        app.geometry("250x575")
         frame = Frame(app)
         frame.pack(side=tk.BOTTOM, fill=tk.X)
         frame.grid_columnconfigure(0, weight=1)
@@ -70,34 +74,68 @@ class Mastermind(Game):
 
         for i in range(1, 13):
             for j in range(1, 5):
-                button = tk.Button(history, state=tk.DISABLED)
+                button = tk.Button(history, width=5, height=2)
                 self.__tentatives[i - 1].append(button)
                 button.grid(row=i, column=j)
 
-        for i in range(1, 5):
-            button = tk.Button(frame, command=lambda: self.cycle_color(button))
-            selection.append(button)
-            button.grid(row=0, column=i)
-        tk.Button(frame, text="Valider", command=lambda: self.attempt(selection)).grid(row=1, column=2, columnspan=3)
+        b1 = tk.Button(frame, width=5, height=2, command=lambda: self.cycle_color(b1))
+        b2 = tk.Button(frame, width=5, height=2, command=lambda: self.cycle_color(b2))
+        b3 = tk.Button(frame, width=5, height=2, command=lambda: self.cycle_color(b3))
+        b4 = tk.Button(frame, width=5, height=2, command=lambda: self.cycle_color(b4))
+
+        selection.append(b1)
+        selection.append(b2)
+        selection.append(b3)
+        selection.append(b4)
+
+        b1.grid(row=0, column=1)
+        b2.grid(row=0, column=2)
+        b3.grid(row=0, column=3)
+        b4.grid(row=0, column=4)
+
+        tk.Button(frame, text="Valider", width=10, command=lambda: self.attempt(selection)).grid(row=1, column=3, columnspan=2)
 
     def attempt(self, selection: List[tk.Button]):
-        borders = []
+        color = []
         temp = self.__solution.copy()
+        correct = 0
         for i, button in enumerate(selection):
             if button.cget("bg") not in self.__lst_couleurs:
                 pass
             if button.cget("bg") == self.__solution[i]:
-                borders.append("red")
+                temp.remove(button.cget("bg"))
+                color.append("red")
+                correct += 1
             elif button.cget("bg") in temp:
                 temp.remove(button.cget("bg"))
-                borders.append("white")
+                color.append("white")
+            else:
+                color.append("black")
         for i, button in enumerate(self.__tentatives[self.__nb_coups]):
-            button.config(bg=selection[i].cget("bg"), border=borders[i])
+            button.config(bg=selection[i].cget("bg"), activebackground=color[i])
         self.__nb_coups += 1
+        if correct >= 4:
+            res = tk.messagebox.askretrycancel(title="Félicitations", message="Vous avez gagné ! ("
+            + str(self.__nb_coups) + " coups)", parent=self.__app)
+            if res:
+                self.__app.destroy()
+                Mastermind.start()
+            else:
+                self.__app.destroy()
+        if self.__nb_coups == 12:
+            res = tk.messagebox.askretrycancel(title="Dommage...", message="Vous avez perdu...", parent=self.__app)
+            if res:
+                self.__app.destroy()
+                Mastermind.start()
+            else:
+                self.__app.destroy()
 
     def cycle_color(self, button: tk.Button):
         if button.cget("bg") in self.__lst_couleurs:
             i = self.__lst_couleurs.index(button.cget("bg"))
-            button.config(bg=self.__lst_couleurs[i + 1])
+            if i + 1 < len(self.__lst_couleurs):
+                button.config(bg=self.__lst_couleurs[i + 1])
+            else:
+                button.config(bg=self.__lst_couleurs[0])
         else:
             button.config(bg=self.__lst_couleurs[0])
