8ae83633513fefc0a41b210ecd6c6fcb6a7022b9
Sagaia <swann.rerat@gmail.com>
2022-04-13 22:40:30 +0200

    Modification de main: - Chemin d'accès en PurePath

--- minigames/Regles.py
+++ minigames/Regles.py
@@ -1,21 +1,23 @@
-logger = logging.getLogger("PyLog")
- def RechercheMotsclefs() :
+import logging
+def RechercheMotsclefs() :
    chaine = eval(input("Que recherchez-vous ? "))
    fichier = open('ReglesDesJeux.docs','r')
    for l in fichier:
        if chaine in l:
            print (l-2,l-1,l,l+1,l+2)
    fichier.close()
-   return
+
 
 class Regles()
-    def afficher
+    def __init__(self) :
+        self.__app = None
+    def start(self) :
         app = tk.Tk()
         app.title("liste des règles")
         entry = Entry(app)
         entry.pack()
         Affichage = Label(app, text=open('RegledDesJeux.docs',r))
         Affichage.pack()
-        fenetre.mainloop()
-        bouton = Button(fenetre, text="Fermer", command=fenetre.quit)
+        app.mainloop()
+        bouton = Button(app, text="Fermer", command=app.quit)
         bouton.pack()
